9bd7d06af474562dfe3b1cf3e7a682db56d6964b
Sooyeol Lee <leesy0517@gmail.com>
2017-10-18 22:29:34 -0400

    Import error

--- app/src/main/java/net/protyposis/android/spectaculum/sample/immersive/MainActivity.java
+++ app/src/main/java/net/protyposis/android/spectaculum/sample/immersive/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.exoplayer2.*;
